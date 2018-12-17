8e6a52f0278d105c6704185249f570f9f16f60ea
Duy Le <duyvietle13@gmail.com>
2018-12-17 12:27:46 -0800

    second commit

--- server.js
+++ server.js
@@ -15,3 +15,13 @@ app.listen(8080, function(){
 }).on('error', err =>{
   console.error(err);
 });
+
+app.get('/api/notes', (req, res) =>{
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res)=>{
+    const id = req.params.id;
+    const note = data.find(item =>item.id === Number(id));
+  res.json(note);
+});
